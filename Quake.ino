3f31637004dc34c7e5af1f68a57c7cbead4b02c5
shridhay <47260704+shridhay@users.noreply.github.com>
2022-12-02 13:38:28 -0800

    Create Quake.ino

---
+++ Quake.ino
@@ -0,0 +1,235 @@
+// Basic demo for accelerometer/gyro readings from Adafruit ISM330DHCX
+#include <BluetoothSerial.h>
+#include "BluetoothSerial.h"
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_ISM330DHCX.h>
+
+BluetoothSerial SerialBT;
+Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
+byte BTData;
+
+// For SPI mode, we need a CS pin
+#define LSM_CS 10
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+
+/* Check if Bluetooth configurations are enabled in the SDK */
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+Adafruit_ISM330DHCX ism330dhcx;
+void setup(void) {
+    
+  pixels.begin();
+  Serial.begin(115200);
+  SerialBT.begin("Quake Sensor");
+  Serial.println("Bluetooth Started! Ready to pair...");
+  
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit ISM330DHCX test!");
+
+  Wire1.setPins(SDA1, SCL1);
+  if (!ism330dhcx.begin_I2C(0x6A, &Wire1)) {
+    Serial.println("Failed to find ISM330DHCX chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("ISM330DHCX Found!");
+
+  // ism330dhcx.setAccelRange(LSM6DS_ACCEL_RANGE_2_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (ism330dhcx.getAccelRange()) {
+  case LSM6DS_ACCEL_RANGE_2_G:
+    Serial.println("+-2G");
+    break;
+  case LSM6DS_ACCEL_RANGE_4_G:
+    Serial.println("+-4G");
+    break;
+  case LSM6DS_ACCEL_RANGE_8_G:
+    Serial.println("+-8G");
+    break;
+  case LSM6DS_ACCEL_RANGE_16_G:
+    Serial.println("+-16G");
+    break;
+  }
+
+  // ism330dhcx.setGyroRange(LSM6DS_GYRO_RANGE_250_DPS);
+  Serial.print("Gyro range set to: ");
+  switch (ism330dhcx.getGyroRange()) {
+  case LSM6DS_GYRO_RANGE_125_DPS:
+    Serial.println("125 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_250_DPS:
+    Serial.println("250 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_500_DPS:
+    Serial.println("500 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_1000_DPS:
+    Serial.println("1000 degrees/s");
+    break;
+  case LSM6DS_GYRO_RANGE_2000_DPS:
+    Serial.println("2000 degrees/s");
+    break;
+  case ISM330DHCX_GYRO_RANGE_4000_DPS:
+    Serial.println("4000 degrees/s");
+    break;
+  }
+
+  // ism330dhcx.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (ism330dhcx.getAccelDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  // ism330dhcx.setGyroDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Gyro data rate set to: ");
+  switch (ism330dhcx.getGyroDataRate()) {
+  case LSM6DS_RATE_SHUTDOWN:
+    Serial.println("0 Hz");
+    break;
+  case LSM6DS_RATE_12_5_HZ:
+    Serial.println("12.5 Hz");
+    break;
+  case LSM6DS_RATE_26_HZ:
+    Serial.println("26 Hz");
+    break;
+  case LSM6DS_RATE_52_HZ:
+    Serial.println("52 Hz");
+    break;
+  case LSM6DS_RATE_104_HZ:
+    Serial.println("104 Hz");
+    break;
+  case LSM6DS_RATE_208_HZ:
+    Serial.println("208 Hz");
+    break;
+  case LSM6DS_RATE_416_HZ:
+    Serial.println("416 Hz");
+    break;
+  case LSM6DS_RATE_833_HZ:
+    Serial.println("833 Hz");
+    break;
+  case LSM6DS_RATE_1_66K_HZ:
+    Serial.println("1.66 KHz");
+    break;
+  case LSM6DS_RATE_3_33K_HZ:
+    Serial.println("3.33 KHz");
+    break;
+  case LSM6DS_RATE_6_66K_HZ:
+    Serial.println("6.66 KHz");
+    break;
+  }
+
+  ism330dhcx.configInt1(false, false, true); // accelerometer DRDY on INT1
+  ism330dhcx.configInt2(false, true, false); // gyro DRDY on INT2
+
+}
+
+void loop() {
+
+  Serial.print("Bootup Successful");
+  
+  if (SerialBT.available()){
+
+    BTData = SerialBT.read();
+    Serial.write(BTData);
+    
+  }
+
+  if (SerialBT.connected()){
+      
+    pixels.setPixelColor(0, pixels.Color(0, 0, 255));
+    pixels.show();
+    
+  }
+
+  if (!SerialBT.connected()){
+      
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+    pixels.show();
+    
+  }
+  
+  //  /* Get a new normalized sensor event */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  ism330dhcx.getEvent(&accel, &gyro, &temp);
+
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print("\t\tAccel X: ");
+  Serial.print(accel.acceleration.x);
+  Serial.print(" \tY: ");
+  Serial.print(accel.acceleration.y);
+  Serial.print(" \tZ: ");
+  Serial.print(accel.acceleration.z);
+  Serial.println(" m/s^2 ");
+
+  SerialBT.print("\t\tAccel X: ");
+  SerialBT.print(accel.acceleration.x);
+  SerialBT.print(" \tY: ");
+  SerialBT.print(accel.acceleration.y);
+  SerialBT.print(" \tZ: ");
+  SerialBT.print(accel.acceleration.z);
+  SerialBT.println(" m/s^2 ");
+
+  /* Display the results (rotation is measured in rad/s) */
+  Serial.print("\t\tGyro X: ");
+  Serial.print(gyro.gyro.x);
+  Serial.print(" \tY: ");
+  Serial.print(gyro.gyro.y);
+  Serial.print(" \tZ: ");
+  Serial.print(gyro.gyro.z);
+  Serial.println(" radians/s ");
+  Serial.println();
+
+  SerialBT.print("\t\tGyro X: ");
+  SerialBT.print(gyro.gyro.x);
+  SerialBT.print(" \tY: ");
+  SerialBT.print(gyro.gyro.y);
+  SerialBT.print(" \tZ: ");
+  SerialBT.print(gyro.gyro.z);
+  SerialBT.println(" radians/s ");
+  SerialBT.println();
+
+  delay(1000);
+}
